fbc500c41e577424c0b14d886a13991f00733286
dapplion <guillem.lion.benosa@gmail.com>
2020-10-28 17:29:51 +0100

    Split NodeJS and browser setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const crypto = require('crypto')
+const createBlsModule = require(`./bls_c.js`)
+const blsSetupFactory = require('./bls')
+
+const bls = {}
+
+const getRandomValues = crypto.randomFillSync
+blsSetupFactory(createBlsModule, getRandomValues, bls)
+
+module.exports = bls;

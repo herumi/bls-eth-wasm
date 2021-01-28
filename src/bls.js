30d6390653e4012d9676e0f72232c8f08ff1ed51
MITSUNARI Shigeo <herumi@nifty.com>
2021-01-29 05:05:32 +0900

    fix test

--- src/bls.js
+++ src/bls.js
@@ -109,7 +109,7 @@ const _blsSetupFactory = (createModule, getRandomValues) => {
         mod.HEAP8.set(buf, pos)
         const r = func(x, pos, buf.length)
         _free(pos)
-        if (r === 0) throw new Error('err _wrapDeserialize', buf)
+        if (r === 0 || r !== buf.length) throw new Error('err _wrapDeserialize', buf)
       }
     }
     /*

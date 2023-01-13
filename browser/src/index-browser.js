01ed01c6bc663b4400fdff16ff634b97b74eb8c9
MITSUNARI Shigeo <herumi@nifty.com>
2023-01-13 18:01:22 +0900

    remove self.msCrypto for IE11

--- browser/src/index-browser.js
+++ browser/src/index-browser.js
@@ -1,6 +1,6 @@
 const createModule = require('../../src/bls_c.js')
 const blsSetupFactory = require('../../src/bls.js')
-const crypto = self.crypto || self.msCrypto
+const crypto = self.crypto
 
 const getRandomValues = x => crypto.getRandomValues(x)
 const bls = blsSetupFactory(createModule, getRandomValues)

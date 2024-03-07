8c4ba7a631dd1f213ed399797a8bbc412676db09
Julien Eluard <julien.eluard@gmail.com>
2024-03-07 17:34:31 +0100

    chore: cleanup

--- browser/src/index-browser.js
+++ browser/src/index-browser.js
@@ -1,9 +1,5 @@
 const createModule = require('../../src/bls_c.js')
 const blsSetupFactory = require('../../src/bls.js')
-const crypto = self.crypto
 
-const getRandomValues = x => crypto.getRandomValues(x)
-const bls = blsSetupFactory(createModule, getRandomValues)
-
-module.exports = bls
+module.exports = blsSetupFactory(createModule)
 
